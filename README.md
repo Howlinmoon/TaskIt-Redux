de8d7150bad8f8dc149ae26b721fe98ee7ea14fb
Jim Veneskey <github@strifecrafter.com>
2015-05-27 13:01:47 -0400

    Extended the view controller by adding two new protocols and implementing their required functions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TaskIt-Redux
+
+Left off here:  http://bitfountain.io/courses/complete-ios8/lectures/4256
+Need to load that page to continue
+
--- Taskit-Redux/ViewController.swift
+++ Taskit-Redux/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Methods required by the UITableViewDataSource protocol we included
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    // Method required by UITableViewDelegate
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
+        
+    }
 
 }
 
