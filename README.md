b424fead079c1b1a1cfc2c8b867a8b1d9ac5df48
Jim Veneskey <github@strifecrafter.com>
2015-05-28 12:28:56 -0400

    Updated some connections in the storyboard

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TaskIt-Redux
 
-Left off here:  http://bitfountain.io/courses/complete-ios8/lectures/4256
+Finished this: http://bitfountain.io/courses/complete-ios8/lectures/4137
+
 
--- Taskit-Redux/ViewController.swift
+++ Taskit-Redux/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.tableView.dataSource = self
+        //self.tableView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
