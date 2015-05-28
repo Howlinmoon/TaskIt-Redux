f082e9121862054cbd592be37766077aa294809b
Jim Veneskey <github@strifecrafter.com>
2015-05-28 12:23:00 -0400

    Hooked up our tableview outlet to the view controller

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # TaskIt-Redux
 
 Left off here:  http://bitfountain.io/courses/complete-ios8/lectures/4256
-Need to load that page to continue
 
--- Taskit-Redux/ViewController.swift
+++ Taskit-Redux/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,13 +24,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
 
-    // Methods required by the UITableViewDataSource protocol we included
+    // Method required by the UITableViewDataSource protocol we included
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 5
         
     }
     
+    // Method required by the UITableViewDataSource protocol we included
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         return UITableViewCell()
